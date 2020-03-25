21f129afe50c984f468399dc7501ed12ff67fa64
yevhenii <testjokerqwerty@gmail.com>
2020-03-26 00:51:54 +0300

    result

--- server/routes/pay.Routes.js
+++ server/routes/pay.Routes.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
 const payController = require("../controller/pay.Controller")
+const resultController = require("../controller/result.controll")
 const router = Router()
 
 
 router.post("/pay", payController)
+router.post("result", resultController)
 
 
 module.exports = router
